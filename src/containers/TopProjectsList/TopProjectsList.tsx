12b27c5a421d094f4e7e48e01e976f8c3aeb3c2e
aryan-kaushik-12 <aryan.kaushik@joshtechnologygroup.com>
2026-03-14 18:03:02 +0530

    FS_04: Rebased

--- src/containers/TopProjectsList/TopProjectsList.tsx
+++ src/containers/TopProjectsList/TopProjectsList.tsx
@@ -25,8 +25,13 @@ import { TopProjectListsProps } from './TopProjectsList.types';
 
 export const TopProjectsList = ({ onAddClick }: TopProjectListsProps) => {
     const navigate = useNavigate();
-    const { data: topProjects, isLoading } = useGetProjectsQuery();
-    const projectsData = topProjects?.data ?? [];
+    const { data: topProjects, isLoading } = useGetProjectsQuery({
+        limit: 5,
+        offset: 0,
+        ordering: undefined,
+        filter: {},
+    });
+    const projectsData = topProjects?.data.results ?? [];
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
